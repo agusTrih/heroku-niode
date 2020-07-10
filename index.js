633dd2835ae6b9bc826e2731af665ea061619df5
agusTrih <ant3dong@gmail.com>
2020-07-10 11:01:22 +0700

    add file to push heroku

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 
 const port = process.env.port || 3001;
-app.get("/", (req, res) => res.send("Hello broth!"));
+const path = require("path");
 
+// membaca  file external
+app.use(express.static("views"));
+
+// untuk menampilkan file html
+app.get("/products", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/product.html"));
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/home.html"));
+});
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
 );
