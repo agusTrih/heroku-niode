ca32b48228e74ca7b49dfb307e0158db45e453f7
agusTrih <ant3dong@gmail.com>
2020-07-10 11:18:28 +0700

    push

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 3001;
+const port = process.env.port || 3002;
 const path = require("path");
 
 // membaca  file external
 app.use(express.static("views"));
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/home.html"));
+});
 // untuk menampilkan file html
 app.get("/products", (req, res) => {
     res.sendFile(path.join(__dirname + "/views/product.html"));
 });
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/home.html"));
-});
+
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
 );
